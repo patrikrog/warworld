50e4902041ddb1743801d78f4acba4aa2fd09316
Patrik <patrik@vintergatan.space>
2023-01-31 17:06:26 +0100

    Implement some more weapons

--- add_weapon.py
+++ add_weapon.py
@@ -8,7 +8,9 @@ app_ctx.push()
 
 weapon = [
     Weapon(name='Fists', min_damage=1, max_damage=4, level_required=1, strength_required=0, rob_chance=10, attack_speed=4, cost=0),
-    Weapon(name='Kitchen knife', min_damage=5, max_damage=10, level_required=15, strength_required=25, rob_chance=50, attack_speed=6, cost=1500),
+    Weapon(name='Kitchen knife', min_damage=5, max_damage=10, level_required=15, strength_required=25, rob_chance=50, attack_speed=6, cost=500),
+    Weapon(name='Chainsaw', min_damage=12, max_damage=16, level_required=30, strength_required=50, rob_chance=100, attack_speed=8, cost=1200),
+    Weapon(name='Luger', min_damage=20, max_damage=30, level_required=50, strength_required=100, rob_chance=180, attack_speed=4, cost=2400),
 ]
 
 [shared.db.session.add(x) for x in weapon]
